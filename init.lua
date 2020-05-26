52b321af7f11aab1e8a9d606bf6f190d0fd1518d
Sebastian Kylander <developer@gaztin.com>
2020-05-26 23:30:32 +0200

    Add CMake global constants

--- init.lua
+++ init.lua
@@ -6,6 +6,18 @@ require 'parser/directory'
 local m      = premake.extensions.impcmake
 local parser = p.extensions.impcmake.parser
 
+-- Constants
+m.ON       = 'ON'
+m.YES      = 'YES'
+m.TRUE     = 'TRUE'
+m.Y        = 'Y'
+m.OFF      = 'OFF'
+m.NO       = 'NO'
+m.FALSE    = 'FALSE'
+m.N        = 'N'
+m.IGNORE   = 'IGNORE'
+m.NOTFOUND = 'NOTFOUND'
+
 function cmake_project( filePath )
 	parser.directory.parse( path.rebase( filePath, '.', 'parser' ) )
 end
