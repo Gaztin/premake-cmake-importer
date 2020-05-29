1c51e8e8b1f3f8c3ee51aa8a479548244dcd6b89
Sebastian Kylander <developer@gaztin.com>
2020-05-30 01:06:35 +0200

    Implement command `target_compile_definitions`

--- parser/directory.lua
+++ parser/directory.lua
@@ -405,6 +405,42 @@ function directory.deserializeProject( content, baseDir )
 			-- Restore scope
 			p.api.scope.project = currentProject
 
+		elseif( cmd.name == 'target_compile_definitions' ) then
+			local arguments      = cmd.arguments
+			-- According to the docs, cannot be an alias target
+			local targetName     = table.remove( arguments, 1 )
+			local projectToAmend = p.workspace.findproject( p.api.scope.workspace, targetName )
+			local allowedScopes  = { 'INTERFACE', 'PUBLIC', 'PRIVATE' }
+			local i              = 0
+
+			while( i < #arguments ) do
+				i = i + 1
+
+				if( table.contains( allowedScopes, arguments[ i ] ) ) then
+					local items = { }
+
+					while( ( i < #arguments ) and ( not table.contains( allowedScopes, arguments[ i + 1 ] ) ) ) do
+						i = i + 1
+
+						local item = arguments[ i ]
+
+						-- Remove leading '-D'
+						item = string.gsub( item, '-D', '', 1 )
+
+						-- Ignore empty items
+						local isEmpty = ( string.len( item ) == 0 ) or
+						                ( ( string.sub( item, 1, 1 ) == '"' ) and
+						                  ( string.sub( item, 2, 2 ) == '"' ) )
+
+						if( not isEmpty ) then
+							table.insert( items, arguments[ i ] )
+						end
+					end
+
+					defines( items )
+				end
+			end
+
 		elseif( cmd.name == 'install' ) then
 
 			-- Skip installation rules
