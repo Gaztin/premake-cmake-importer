dd066ad3dfe46ec5a6c352beedf4be552204274f
Sebastian Kylander <developer@gaztin.com>
2020-05-30 02:16:01 +0200

    Add helper function to unquote strings to raw

--- utility.lua
+++ utility.lua
@@ -4,3 +4,11 @@ local m = premake.extensions.impcmake
 function m.isStringLiteral( str )
 	return ( str:startswith( '"' ) and str:endswith( '"' ) )
 end
+
+function m.toRawString( str )
+	if( m.isStringLiteral( str ) ) then
+		return str:gsub( '^"(.*)"', '%1' )
+	else
+		return str
+	end
+end
