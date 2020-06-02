ef58e997b77d3d1a235eb89cb43763a552c845c3
Sebastian Kylander <developer@gaztin.com>
2020-06-02 23:13:33 +0200

    Find FindX.cmake file and report

--- parser/directory.lua
+++ parser/directory.lua
@@ -609,6 +609,49 @@ function directory.deserializeProject( content, baseDir )
 
 			propertyHandler( meta, property, values )
 
+		elseif( cmd.name == 'find_package' ) then
+
+			if( os.isfile( m.CMAKE_MODULES_CACHE_AVAILABLE ) ) then
+				-- TODO: Full signature
+				-- TODO: COMPONENTS and OPTIONAL_COMPONENTS
+				local arguments        = cmd.arguments
+				local possible_options = { 'EXACT', 'QUIET', 'MODULE', 'REQUIRED', 'NO_POLICY_SCOPE' }
+				local packageName      = table.remove( cmd.arguments, 1 )
+				local version          = iif( arguments[ 1 ] and not table.contains( possible_options, arguments[ 1 ] ), table.remove( arguments, 1 ), '0.0.0' )
+				local options          = table.intersect( possible_options, arguments )
+
+				if( table.contains( options, 'EXACT' ) ) then
+					-- TODO: EXACT
+				end
+				if( table.contains( options, 'QUIET' ) ) then
+					-- TODO: QUET
+				end
+				if( table.contains( options, 'MODULE' ) ) then
+					-- TODO: MODULE
+				end
+				if( table.contains( options, 'REQUIRED' ) ) then
+					-- TODO: REQUIRED
+				end
+				if( table.contains( options, 'NO_POLICY_SCOPE' ) ) then
+					-- TODO: NO_POLICY_SCOPE
+				end
+
+				local fileName = string.format( 'Find%s.cmake', packageName )
+				local filePath = path.join( m.CMAKE_MODULES_CACHE, fileName )
+
+				if( os.isfile( filePath ) ) then
+					term.pushColor( term.green )
+					printf( 'Found module "%s" at "%s"', packageName, filePath )
+					term.popColor()
+
+					-- TODO: Let module do this instead
+					m.scope.variables[ packageName .. '_FOUND' ] = m.YES
+				end
+
+			else
+				p.error( 'CMake module cache is not available for command "%s"', cmd.name )
+			end
+
 		elseif( ( cmd.name == 'if' ) or ( cmd.name == 'elseif' ) ) then
 
 			-- Don't evaluate elseif if any previous tests were successful
