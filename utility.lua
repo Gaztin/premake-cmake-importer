e3c1f40c63d53064de5f0111f5861abb0b4c3268
Sebastian Kylander <developer@gaztin.com>
2020-06-05 00:23:15 +0200

    Resolve environment variables

--- utility.lua
+++ utility.lua
@@ -2,6 +2,7 @@ local p = premake
 local m = p.extensions.impcmake
 
 function m.resolveVariables( str )
+	-- Global variables
 	repeat
 		st, en = string.find( str, '${%S+}' )
 
@@ -18,6 +19,25 @@ function m.resolveVariables( str )
 			end
 		end
 	until( st == nil )
+
+	-- Environment variables
+	repeat
+		st, en = string.find( str, '$ENV{%S+}' )
+
+		if( st ~= nil ) then
+			local var   = string.sub( str, st + 5, en - 1 )
+			local value = os.getenv( var )
+
+			if( value ~= nil ) then
+				local detokenizedValue = p.detoken.expand( value, vars )
+				str = string.sub( str, 1, st - 1 ) .. detokenizedValue .. string.sub( str, en + 1 )
+			else
+				str = string.sub( str, 1, st - 1 ) .. string.sub( str, en + 1 )
+			end
+		end
+	until( st == nil )
+
+	-- TODO: $CACHE{%S+}
 
 	return str
 end
