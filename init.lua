8bf184151bfcdc67b893f45aaf56948291812776
Sebastian Kylander <developer@gaztin.com>
2020-06-06 20:59:54 +0200

    Move script parser to script.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,13 @@
 local p               = premake
 p.extensions.impcmake = { }
+local m               = premake.extensions.impcmake
 
 require 'api'
 require 'utility'
 require 'cmake_modules_cache'
 
+require 'parser/script'
 require 'parser/directory'
-
-local m      = premake.extensions.impcmake
-local parser = p.extensions.impcmake.parser
 
 m._VERSION               = '1.0.0'
 m._LASTEST_CMAKE_VERSION = '3.17.3'
@@ -26,5 +25,5 @@ m.IGNORE   = 'IGNORE'
 m.NOTFOUND = 'NOTFOUND'
 
 function cmake_project( filePath )
-	parser.directory.parse( path.rebase( filePath, '.', 'parser' ) )
+	m.parseDirectory( path.rebase( filePath, '.', 'parser' ) )
 end
