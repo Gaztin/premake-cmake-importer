623dd8b2a362325f3204bf2644903282aa820757
Sebastian Kylander <developer@gaztin.com>
2020-06-13 23:07:58 +0200

    Parse the 'find_path' full command syntax

--- parser/command_list.lua
+++ parser/command_list.lua
@@ -521,6 +521,105 @@ executors[ 'find_package' ] = function( cmd )
 	end
 end
 
+executors[ 'find_path' ] = function( cmd )
+	local possibleOptions    = { 'HINTS', 'PATHS', 'PATH_SUFFIXES', 'DOC', 'REQUIRED',
+	                            'NO_DEFAULT_PATH', 'NO_PACKAGE_ROOT_PATH', 'NO_CMAKE_PATH',
+	                            'NO_CMAKE_ENVIRONMENT_PATH', 'NO_SYSTEM_ENVIRONMENT_PATH',
+	                            'NO_CMAKE_SYSTEM_PATH', 'CMAKE_FIND_ROOT_PATH_BOTH',
+	                            'ONLY_CMAKE_FIND_ROOT_PATH', 'NO_CMAKE_FIND_ROOT_PATH' }
+	local arguments          = table.arraycopy( cmd.arguments )
+	local var                = table.remove( arguments, 1 )
+	local names              = { }
+	local hints              = { }
+	local paths              = { }
+	local subDirs            = { }
+	local docString          = ''
+	local isRequired         = false
+	local searchPackageRoot  = m.isTrue( m.expandVariable( 'CMAKE_FIND_USE_PACKAGE_ROOT_PATH', iif( m.currentPackage ~= nil, m.TRUE, m.FALSE ) ) )
+	local searchCMakePath    = m.isTrue( m.expandVariable( 'CMAKE_FIND_USE_CMAKE_PATH', m.TRUE ) )
+	local searchCMakeEnvPath = m.isTrue( m.expandVariable( 'CMAKE_FIND_USE_CMAKE_ENVIRONMENT_PATH', m.TRUE ) )
+	local searchSysEnvPath   = m.isTrue( m.expandVariable( 'CMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH', m.TRUE ) )
+	local searchCMakeSysPath = m.isTrue( m.expandVariable( 'CMAKE_FIND_USE_CMAKE_SYSTEM_PATH', m.TRUE ) )
+	local useFindRootPathVar = true
+	local searchOnlyRoots    = false
+
+	-- Names
+	if( arguments[ 1 ] == 'NAMES' ) then
+		table.remove( arguments, 1 )
+		while( not table.contains( possibleOptions, arguments[ 1 ] ) ) do
+			table.insert( names, table.remove( arguments, 1 ) )
+		end
+	else
+		table.insert( names, table.remove( arguments, 1 ) )
+	end
+
+	-- Parse options
+	while( #arguments > 0 ) do
+		local option = table.remove( arguments, 1 )
+		if( option == 'HINTS' ) then
+			-- Directories to search in
+			while( not table.contains( possibleOptions, arguments[ 1 ] ) ) do
+				local arg = table.remove( arguments, 1 )
+				if( arg == 'ENV' ) then
+					local env = table.remove( arguments, 1 )
+					table.insert( hints, os.getenv( env ) )
+				else
+					table.insert( hints, arg )
+				end
+			end
+		elseif( option == 'PATHS' ) then
+			-- Directories to search in (prioritized last)
+			while( not table.contains( possibleOptions, arguments[ 1 ] ) ) do
+				local arg = table.remove( arguments, 1 )
+				if( arg == 'ENV' ) then
+					local env = table.remove( arguments, 1 )
+					table.insert( paths, os.getenv( env ) )
+				else
+					table.insert( paths, arg )
+				end
+			end
+		elseif( option == 'PATH_SUFFIXES' ) then
+			-- Subdirectories to search in
+			while( not table.contains( possibleOptions, arguments[ 1 ] ) ) do
+				local arg = table.remove( arguments, 1 )
+				table.insert( subDirs, arg )
+			end
+		elseif( option == 'DOC' ) then
+			-- Documentation string
+			local arg = table.remove( arguments, 1 )
+			docString = arg
+		elseif( option == 'REQUIRED' ) then
+			-- Abort if nothing is found
+			local arg = table.remove( arguments, 1 )
+			isRequired = m.isTrue( arg )
+		elseif( option == 'NO_DEFAULT_PATH' ) then
+			searchPackageRoot  = false
+			searchCMakePath    = false
+			searchCMakeEnvPath = false
+			searchSysEnvPath   = false
+			searchCMakeSysPath = false
+		elseif( option == 'NO_PACKAGE_ROOT_PATH' ) then
+			searchPackageRoot = false
+		elseif( option == 'NO_CMAKE_PATH' ) then
+			searchCMakePath = false
+		elseif( option == 'NO_CMAKE_ENVIRONMENT_PATH' ) then
+			searchCMakeEnvPath = false
+		elseif( option == 'NO_SYSTEM_ENVIRONMENT_PATH' ) then
+			searchSysEnvPath = false
+		elseif( option == 'NO_CMAKE_SYSTEM_PATH' ) then
+			searchCMakeSysPath = false
+		elseif( option == 'CMAKE_FIND_ROOT_PATH_BOTH' ) then
+			-- Don't need to change any settings
+		elseif( option == 'ONLY_CMAKE_FIND_ROOT_PATH' ) then
+			searchOnlyRoots = true
+		elseif( option == 'NO_CMAKE_FIND_ROOT_PATH' ) then
+			useFindRootPathVar = false
+		end
+	end
+
+	-- TODO: Apply options
+end
+
 executors[ 'if' ] = function( cmd, condscope__refwrap )
 	if( cmd.name == 'if' ) then
 		local newscope         = { }
