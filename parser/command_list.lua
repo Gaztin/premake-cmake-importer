6e55961aeba9b8ab650d43aea6f1cb2df2c400d7
Sebastian Kylander <developer@gaztin.com>
2020-06-14 20:17:16 +0200

    Implement 'include' command

--- parser/command_list.lua
+++ parser/command_list.lua
@@ -538,6 +538,45 @@ executors[ 'find_program' ] = function( cmd )
 	m.findPath( cmd, 'PROGRAM', iif( os.ishost( 'windows' ), '.exe', nil ) )
 end
 
+executors[ 'include' ] = function( cmd )
+	local arguments = table.arraycopy( cmd.arguments )
+	local file      = table.remove( arguments, 1 )
+	local required  = true
+	local resultVar = nil
+
+	while( #arguments > 0 ) do
+		local arg = table.remove( arguments, 1 )
+
+		if( arg == 'OPTIONAL' ) then
+			required = false
+
+		elseif( arg == 'RESULT_VARIABLE' ) then
+			local var = table.remove( arguments, 1 )
+
+			resultVar = var
+
+		elseif( arg == 'NO_POLICY_SCOPE' ) then
+			-- TODO: CMake Policies
+		end
+	end
+
+	if( os.isfile( file ) ) then
+		m.parseScript( file )
+
+		if( resultVar ) then
+			cmakecache {
+				[ resultVar ] = file,
+			}
+		end
+	else
+		-- TODO: May be module, try to find <modulename>.cmake
+
+		if( required ) then
+			p.error( 'Failed to find the file "%s" for command "include"', file )
+		end
+	end
+end
+
 executors[ 'if' ] = function( cmd, condscope__refwrap )
 	if( cmd.name == 'if' ) then
 		local newscope         = { }
