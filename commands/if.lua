c26739ae4d56640769c9bf37971f18ce49f48785
Sebastian Kylander <developer@gaztin.com>
2021-03-04 12:53:46 +0100

    Fix if commands

--- commands/if.lua
+++ commands/if.lua
@@ -2,26 +2,28 @@ local p = premake
 local m = p.extensions.impcmake
 
 local function endif( commands, data )
-	local i = 1
-	repeat
-		if( not m.groups.recording ) then
-			if( commands[ i ].name == 'elseif' ) then
-				data.handled   = data.handled or data.lastCheck
-				data.lastCheck = m.conditions.evalExpression( table.concat( commands[ i ].arguments, ' ' ) )
-				i              = i + 1
-			elseif( commands[ i ].name == 'else' ) then
-				data.handled   = data.handled or data.lastCheck
-				data.lastCheck = not data.lastCheck
-				i              = i + 1
-			end
+	local nestLevel = 0
+	for i,command in ipairs( commands ) do
+		if( command.name == 'if' ) then
+			nestLevel = nestLevel + 1
+		elseif( command.name == 'endif' ) then
+			nestLevel = nestLevel - 1
 		end
 
-		if( data.lastCheck and not data.handled ) then
-			m.executeCommand( commands[ i ] )
+		if( not data.handled ) then
+			if( nestLevel == 0 and command.name == 'elseif' ) then
+				data.handled   = data.handled or data.lastCheck
+				data.lastCheck = m.conditions.evalExpression( table.concat( command.arguments, ' ' ) )
+			elseif( nestLevel == 0 and command.name == 'else' ) then
+				data.handled   = data.handled or data.lastCheck
+				data.lastCheck = not data.lastCheck
+			else
+				if( data.lastCheck ) then
+					m.executeCommand( command )
+				end
+			end
 		end
-		
-		i = i + 1
-	until( i > #commands )
+	end
 end
 
 m.commands[ 'if' ] = function( cmd )
