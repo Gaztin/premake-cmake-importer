f9181208e90d4c30c7eae50aff275c8df78b43ca
Sebastian Kylander <developer@gaztin.com>
2021-03-04 21:06:12 +0100

    Fix functions and macros not setting ARG fields

--- commands/function.lua
+++ commands/function.lua
@@ -5,9 +5,25 @@ local function endfunction( commands, data )
 	m.commands[ data.name:lower() ] = function( cmd )
 		local scope = m.scope.push()
 
+		-- Make sure number of arguments are equal or more than the number of parameters
+		if( #cmd.arguments < #data.parameters ) then
+			p.error( 'Function "%s" was called with too few arguments. Expected %d but received %d!', cmd.name, #data.parameters, #cmd.arguments )
+		end
+
 		scope.variables[ 'CMAKE_CURRENT_FUNCTION' ]           = cmd.name
 		scope.variables[ 'CMAKE_CURRENT_FUNCTION_LIST_DIR' ]  = scope.parent.variables[ 'CMAKE_CURRENT_LIST_DIR' ]
 		scope.variables[ 'CMAKE_CURRENT_FUNCTION_LIST_FILE' ] = scope.parent.variables[ 'CMAKE_CURRENT_LIST_FILE' ]
+		scope.variables[ 'ARGC' ]                             = #cmd.arguments
+		scope.variables[ 'ARGV' ]                             = table.concat( cmd.arguments, ';' )
+		scope.variables[ 'ARGN' ]                             = table.concat( { table.unpack( cmd.arguments, #data.parameters + 1 ) }, ';' )
+
+		for i,param in ipairs( data.parameters ) do
+			scope.variables[ param ] = cmd.arguments[ i ]
+		end
+
+		for i,arg in ipairs( cmd.arguments ) do
+			scope.variables[ 'ARGV' .. ( i - 1 ) ] = arg
+		end
 
 		for i,command in ipairs( commands ) do
 			m.executeCommand( command )
@@ -19,7 +35,8 @@ end
 
 m.commands[ 'function' ] = function( cmd )
 	local data = {
-		name = cmd.arguments[ 1 ],
+		name       = cmd.arguments[ 1 ],
+		parameters = { table.unpack( cmd.arguments, 2 ) }
 	}
 
 	m.groups.push( 'function', 'endfunction', endfunction, data )
--- commands/macro.lua
+++ commands/macro.lua
@@ -1,9 +1,46 @@
 local p = premake
 local m = p.extensions.impcmake
 
+local function specializeCommands( cmd, commands, params )
+	-- Make sure the number of arguments are equal or more than the number of parameters
+	if( #cmd.arguments < #params ) then
+		p.error( 'Macro "%s" was called with too few arguments. Expected %d but received %d!', cmd.name, #params, #cmd.arguments )
+	end
+
+	local replacements = {
+		[ '${ARGC}' ] = #cmd.arguments,
+		[ '${ARGV}' ] = table.concat( cmd.arguments, ';' ),
+		[ '${ARGN}' ] = table.concat( { table.unpack( cmd.arguments, #params + 1 ) }, ';' ),
+	}
+
+	for i,param in ipairs( params ) do
+		replacements[ '${' .. param .. '}' ] = args[ i ]
+	end
+
+	for i,arg in ipairs( cmd.arguments ) do
+		replacements[ '${ARGV' .. i .. '}' ] = arg
+	end
+
+	for i=1,#commands do
+		for replacee,replacement in ipairs( replacements ) do
+			commands[ i ] = string.gsub( commands[ i ], replacee, replacement )
+		end
+	end
+
+	return commands
+end
+
 local function endmacro( commands, data )
 	m.commands[ data.name ] = function( cmd )
-		for i,command in ipairs( commands ) do
+		local specializedCommands = specializeCommands( cmd, commands, data.parameters )
+
+		-- TODO: Remove after making sure this works
+		print( '[MACRO]:' )
+		for i=1,#commands do
+			print( '\t' .. commands[ i ] .. '  -->  ' .. specializedCommands[ i ] )
+		end
+
+		for i,command in ipairs( specializedCommands ) do
 			m.executeCommand( command )
 		end
 	end
@@ -11,7 +48,8 @@ end
 
 m.commands[ 'macro' ] = function( cmd )
 	local data = {
-		name = cmd.arguments[ 1 ],
+		name       = cmd.arguments[ 1 ],
+		parameters = { table.unpack( cmd.arguments, 2 ) },
 	}
 
 	m.groups.push( 'macro', 'endmacro', endmacro, data )
